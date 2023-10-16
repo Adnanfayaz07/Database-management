e08e3045d0b0d9070055d62f3a73417db3ad0af9
Adnanfayaz07 <adnanfayaz581@gmail.com>
2023-10-16 15:02:34 +0530

    first commit

---
+++ routes/route.js
@@ -0,0 +1,13 @@
+const express=require('express')
+const router=express.Router()
+const dynamicController=require('../controllers/dynamiccontroller')
+
+router.post('/create-table',dynamicController.createTable)
+router.post('/insert-record',dynamicController.insertRecord)
+router.get('/get-models',dynamicController.getModels)
+router.get('/get-records',dynamicController.getRecords)
+router.delete('/delete-record/:id',dynamicController.deleteRecord)
+
+
+module.exports=router
+
